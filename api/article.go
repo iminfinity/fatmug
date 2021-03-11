e213624ae906192f9f9937cbf4e14a6f82145ae1
iminfinity <this.is.me.infinity@gmail.com>
2021-03-11 16:20:17 +0530

    updated user, articles and userArticle

--- api/article.go
+++ api/article.go
@@ -1,112 +1,17 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/iminfinity/fatmug/backend/models"
-	"gopkg.in/mgo.v2/bson"
 )
 
-// AddArticle func
-func AddArticle(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Add("content-type", "application/json")
-	params := mux.Vars(r)
-	var user models.User
-	userId := params["userId"]
-	err = usersCollection.FindOne(ctx, bson.M{"userId": userId}).Decode(&user)
+// AddArticleToDB func
+func AddArticleToDB(newArticle models.Article) {
+	_, err := articlesCollection.InsertOne(ctx, newArticle)
 	if err != nil {
-		http.Error(rw, "Error getting user", http.StatusInternalServerError)
-		fmt.Println("Error getting user")
+		fmt.Println("Error adding user")
 		return
 	}
-
-	var newArticle models.Article
-
-	err = json.NewDecoder(r.Body).Decode(&newArticle)
-
-	if err != nil {
-		http.Error(rw, "Error decoding article", http.StatusInternalServerError)
-		fmt.Println("Error decoding article")
-		return
-	}
-
-	user.Articles = append(user.Articles, newArticle)
-
-	_, err = usersCollection.UpdateOne(ctx, bson.M{"userId": userId}, bson.M{"$set": user})
-	if err != nil {
-		http.Error(rw, "Error saving user", http.StatusInternalServerError)
-		fmt.Println("Error saving user")
-		return
-	}
-
-	fmt.Fprintf(rw, "Article saved to database successfully")
-	fmt.Println("New article saved")
-}
-
-// UpdateArticle func
-func UpdateArticle(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Add("content-type", "application/json")
-	params := mux.Vars(r)
-	userId, articleIndex := params["userId"], params["articleIndex"]
-	var updatedArticle models.Article
-
-	var user models.User
-
-	err = usersCollection.FindOne(ctx, bson.M{"userId": userId}).Decode(&user)
-	if err != nil {
-		http.Error(rw, "Error getting user", http.StatusInternalServerError)
-		fmt.Println("Error getting user")
-		return
-	}
-	index, err := strconv.Atoi(articleIndex)
-	if err != nil {
-		http.Error(rw, "artcile index must be an integer", http.StatusInternalServerError)
-		fmt.Println("artcile index must be an integer")
-		return
-	}
-	user.Articles[index] = updatedArticle
-
-	_, err = usersCollection.UpdateOne(ctx, bson.M{"userId": userId}, bson.M{"$set": user})
-	if err != nil {
-		http.Error(rw, "Error updaing user", http.StatusInternalServerError)
-		fmt.Println("Error updaing user")
-		return
-	}
-
-	fmt.Fprintf(rw, "Article updated")
-	fmt.Println("Article updated")
-}
-
-// RemoveArticle func
-func RemoveArticle(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Add("content-type", "application/json")
-	params := mux.Vars(r)
-	userId, articleIndex := params["userId"], params["articleIndex"]
-	var user models.User
-	err = usersCollection.FindOne(ctx, bson.M{"userId": userId}).Decode(&user)
-	if err != nil {
-		http.Error(rw, "Error getting user", http.StatusInternalServerError)
-		fmt.Println("Error getting user")
-		return
-	}
-	index, err := strconv.Atoi(articleIndex)
-	if err != nil {
-		http.Error(rw, "artcile index must be an integer", http.StatusInternalServerError)
-		fmt.Println("artcile index must be an integer")
-		return
-	}
-	user.Articles = append(user.Articles[:index], user.Articles[index+1:]...)
-	_, err = usersCollection.UpdateOne(ctx, bson.M{"userId": userId}, bson.M{"$set": user})
-	if err != nil {
-		http.Error(rw, "Error updaing user", http.StatusInternalServerError)
-		fmt.Println("Error updaing user")
-		return
-	}
-
-	fmt.Fprintf(rw, "Article Removed")
-	fmt.Println("Article Removed")
+	fmt.Println("Add article to mongo")
 }
--- api/getArticles.go
+++ api/getArticles.go
@@ -1,18 +1,45 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-)
+	"strconv"
 
-// GetUserArticles func
-func GetUserArticles(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "Hello world")
-}
+	"github.com/gorilla/mux"
+	"github.com/iminfinity/fatmug/backend/models"
+	"gopkg.in/mgo.v2/bson"
+)
 
 // GetArticles func
 func GetArticles(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "Hello world")
+	var articles []models.Article
+	cursor, err := articlesCollection.Find(ctx, bson.D{})
+	if err != nil {
+		http.Error(rw, "Error getting articles", http.StatusInternalServerError)
+		fmt.Println("Error getting articles")
+		return
+	}
+	for cursor.Next(ctx) {
+		err = cursor.Decode(&articles[len(articles)-1])
+		if err != nil {
+			continue
+		}
+	}
+
+	params := mux.Vars(r)
+	pageNumber, err := strconv.Atoi(params["pageNumber"])
+	if err != nil {
+		http.Error(rw, "Error formating page number", http.StatusInternalServerError)
+		fmt.Println("Error formating page number")
+	}
+	var currentArticles []models.Article
+
+	for i := 0; i <= 10; i++ {
+		currentArticles = append(currentArticles, articles[(pageNumber-1)*10+i])
+	}
+
+	json.NewEncoder(rw).Encode(currentArticles)
 }
 
 // GetPopularArticles func
