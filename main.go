3f3d8835912958f1128feb49402990046a32c06b
iminfinity <this.is.me.infinity@gmail.com>
2021-03-11 14:21:05 +0530

    added update article, create article and remove article

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 	router.HandleFunc("/add-new-user/{userId}", api.AddNewUser).Methods("POST")
 
 	router.HandleFunc("/add-article/{userId}/", api.AddArticle).Methods("POST")
-	router.HandleFunc("/update-article/{userId}/{articleId}", api.UpdateArticle).Methods("UPDATE")
-	router.HandleFunc("/remove-article/{userId}/{articleId}", api.RemoveArticle).Methods("DELETE")
+	router.HandleFunc("/update-article/{userId}/{articleIndex}", api.UpdateArticle).Methods("UPDATE")
+	router.HandleFunc("/remove-article/{userId}/{articleIndex}", api.RemoveArticle).Methods("DELETE")
 
 	router.HandleFunc("/get-user-articles/{userId}/", api.GetUserArticles).Methods("GET")
 
